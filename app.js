e97fe81a04670c224267704f4695326575a2144c
Ahmed-Shafee <Ahmed_92a@hotmail.com>
2024-07-07 15:47:19 +0300

    updates the files locations in the dirctory

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const bcrypt = require('bcrypt');
 const path = require("path");
 const bodyParser = require('body-parser');
 const users = require('./data').userDB;
-  
+
 const app = express();
 const server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'./')));
+app.use(express.static(path.join(__dirname,'./public')));
 
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'./index.html'));
+    res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
 
